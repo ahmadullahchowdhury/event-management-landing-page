2fcf85ebea4c730e91d84bf02364b5060a94b447
A H M Ahmadullah Chowdhury <kowcher99@gmail.com>
2024-06-12 02:08:17 +0600

    hero hero section

--- view/page/home/hero/index.tsx
+++ view/page/home/hero/index.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
         style={{ backgroundImage: 'url(/image/hero-bg.jpg)' }}
       >
         {/* bg-[#1c809e] */}
-        <div className=' inset-0 bg-black/45 opacity-90  flex items-center justify-center h-[800px] 2xl:min-h-screen 	 backdrop-blur-[2px]'>
+        <div className=' inset-0 flex h-[800px]  items-center justify-center bg-black/45 opacity-90 backdrop-blur-[2px] 	 2xl:min-h-screen'>
           {/* <div className=' grid grid-cols-2 items-center   '>
             <div className='flex items-end  bg-red-500   md:px-16 2xl:px-0'>
               <p className='mx-auto flex items-center gap-2 text-white md:max-w-[800px]'>
@@ -37,28 +37,24 @@ const Hero = () => {
 
           <div className='grid grid-cols-2'>
             <div className='flex items-end justify-center '>
-              
-                <div className='mx-auto flex items-center gap-2 text-white md:max-w-[800px]'>
-                  <FaPlayCircle className='text-red-500'  size={40} />
-                    <p className='text-lg'> PLAY THE VIDEO </p>
-                </div>
-             
+              <div className='mx-auto flex items-center gap-2 text-white md:max-w-[800px]'>
+                <FaPlayCircle className='text-red-500' size={40} />
+                <p className='text-lg'> PLAY THE VIDEO </p>
+              </div>
             </div>
 
-            
-              <div className='   md:px-16 2xl:px-0'>
-                <h2 className='mx-sauto max-w-[334px] text-center text-[28px] font-semibold text-white drop-shadow-2xl md:mx-0 md:max-w-[800px] md:text-left  md:text-[36px] lg:text-[70px] lg:leading-[80px]'>
-                  The UK’s Best Event Management Agency for Corporate
-                </h2>
-                <p className=' mx-auto mt-[14px] max-w-[235px] text-center text-[16px] font-normal text-white md:mx-0 md:mt-3 md:max-w-[680px] md:text-left md:text-base'>
-                  A integer hac in id. Mollis consequat, consectetur egestas in quam. Id velit eleifend tincidunt varius
-                  sagittis sed ut. Pretium, risus ac augue arcu ullamcorper semper integer.
-                </p>
-                <Link href={'/services'} className='mt-[50px] flex justify-center drop-shadow-2xl md:justify-start'>
-                  <div className='inline-flex rounded-lg  border-2  px-5 py-1.5 text-white'>Explore More</div>
-                </Link>
-              </div>
-            
+            <div className='   md:px-16 2xl:px-0'>
+              <h2 className='mx-sauto max-w-[334px] text-center text-[28px] font-semibold text-white drop-shadow-2xl md:mx-0 md:max-w-[800px] md:text-left  md:text-[36px] lg:text-[70px] lg:leading-[80px]'>
+                The UK’s Best Event Management Agency for Corporate
+              </h2>
+              <p className=' mx-auto mt-[14px] max-w-[235px] text-center text-[16px] font-normal text-white md:mx-0 md:mt-3 md:max-w-[680px] md:text-left md:text-base'>
+                A integer hac in id. Mollis consequat, consectetur egestas in quam. Id velit eleifend tincidunt varius
+                sagittis sed ut. Pretium, risus ac augue arcu ullamcorper semper integer.
+              </p>
+              <Link href={'/services'} className='mt-[50px] flex justify-center drop-shadow-2xl md:justify-start'>
+                <div className='inline-flex rounded-lg  border-2  px-5 py-1.5 text-white'>Explore More</div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
