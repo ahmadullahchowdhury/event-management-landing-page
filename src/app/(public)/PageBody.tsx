d5d07dee6e051d98b412bc605934126e2885b127
A H M Ahmadullah Chowdhury <kowcher99@gmail.com>
2024-06-12 02:05:36 +0600

    hero hero section

--- src/app/(public)/PageBody.tsx
+++ src/app/(public)/PageBody.tsx
@@ -2,12 +2,14 @@
 import { Fragment } from 'react';
 import AboutUs from 'view/page/home/about-us';
 import FeaturedWorks from 'view/page/home/featuredWorks';
+import Hero from 'view/page/home/hero';
 import Services from 'view/page/home/services';
 import Testimonials from 'view/page/home/testimonials';
 
 const PageBody = () => {
   return (
     <Fragment>
+      <Hero />
       <AboutUs/>
       <FeaturedWorks />
       <Testimonials />
--- view/page/home/hero/index.tsx
+++ view/page/home/hero/index.tsx
@@ -2,32 +2,63 @@ import { url } from 'inspector';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { FaPlayCircle } from 'react-icons/fa';
+import { PiLayoutFill } from 'react-icons/pi';
 
 const Hero = () => {
   return (
     <div className='relative'>
       <div
         className='relative mx-auto overflow-hidden bg-cover bg-center'
-        style={{ backgroundImage: 'url(/image/home/hero-bg.jpg)' }}
+        style={{ backgroundImage: 'url(/image/hero-bg.jpg)' }}
       >
         {/* bg-[#1c809e] */}
-        <div className='inset-0 bg-[#242a56d7] backdrop-blur-[2px]	 opacity-90'>
-          <div className='mx-auto flex h-[800px] 2xl:min-h-screen max-w-[1280px] flex-col justify-center '>
-            <div className=' md:px-16 2xl:px-0'>
-              <p className='drop-shadow-2xl mx-auto mb-5 max-w-[234px] text-center text-[18px] font-bold text-white md:mx-0 md:max-w-[650px] md:text-left md:text-[26px] lg:text-left lg:text-2xl'>
-              Where Data Meets Precision
+        <div className=' inset-0 bg-black/45 opacity-90  flex items-center justify-center h-[800px] 2xl:min-h-screen 	 backdrop-blur-[2px]'>
+          {/* <div className=' grid grid-cols-2 items-center   '>
+            <div className='flex items-end  bg-red-500   md:px-16 2xl:px-0'>
+              <p className='mx-auto flex items-center gap-2 text-white md:max-w-[800px]'>
+                <FaPlayCircle color='red' size={30} />
+                PLAY THE VIDEO
               </p>
-              <h2 className='drop-shadow-2xl mx-auto max-w-[334px] text-center text-[28px] font-bold text-white md:mx-0 md:max-w-[790px] md:text-left  md:text-[36px] lg:text-[60px] lg:leading-[70px]'>
-              Accuracy You Can Trust, Results You Can See
+            </div>
+            <div className=' bg-red-500  md:px-16 2xl:px-0'>
+              <h2 className='mx-sauto max-w-[334px] text-center text-[28px] font-semibold text-white drop-shadow-2xl md:mx-0 md:max-w-[800px] md:text-left  md:text-[36px] lg:text-[70px] lg:leading-[90px]'>
+                The UK’s Best Event Management Agency for Corporate
               </h2>
               <p className=' mx-auto mt-[14px] max-w-[235px] text-center text-[16px] font-normal text-white md:mx-0 md:mt-3 md:max-w-[680px] md:text-left md:text-base'>
-              We specialize in mastering the intricacies of data to empower your business success. Our precision-driven services ensure flawless data management, enabling you to focus on growth and innovation. Trust us to transform your data into a powerful asset for your business
+                A integer hac in id. Mollis consequat, consectetur egestas in quam. Id velit eleifend tincidunt varius
+                sagittis sed ut. Pretium, risus ac augue arcu ullamcorper semper integer.
               </p>
-
-              <Link href={'/services'} className='mt-[50px] drop-shadow-2xl flex justify-center md:justify-start'>
-                <div className='inline-flex rounded-lg  bg-[#f6c32c] px-10 py-3 text-black'>Explore More</div>
+              <Link href={'/services'} className='mt-[50px] flex justify-center drop-shadow-2xl md:justify-start'>
+                <div className='inline-flex rounded-lg  border-2  px-5 py-1.5 text-white'>Explore More</div>
               </Link>
             </div>
+          </div> */}
+
+          <div className='grid grid-cols-2'>
+            <div className='flex items-end justify-center '>
+              
+                <div className='mx-auto flex items-center gap-2 text-white md:max-w-[800px]'>
+                  <FaPlayCircle className='text-red-500'  size={40} />
+                    <p className='text-lg'> PLAY THE VIDEO </p>
+                </div>
+             
+            </div>
+
+            
+              <div className='   md:px-16 2xl:px-0'>
+                <h2 className='mx-sauto max-w-[334px] text-center text-[28px] font-semibold text-white drop-shadow-2xl md:mx-0 md:max-w-[800px] md:text-left  md:text-[36px] lg:text-[70px] lg:leading-[80px]'>
+                  The UK’s Best Event Management Agency for Corporate
+                </h2>
+                <p className=' mx-auto mt-[14px] max-w-[235px] text-center text-[16px] font-normal text-white md:mx-0 md:mt-3 md:max-w-[680px] md:text-left md:text-base'>
+                  A integer hac in id. Mollis consequat, consectetur egestas in quam. Id velit eleifend tincidunt varius
+                  sagittis sed ut. Pretium, risus ac augue arcu ullamcorper semper integer.
+                </p>
+                <Link href={'/services'} className='mt-[50px] flex justify-center drop-shadow-2xl md:justify-start'>
+                  <div className='inline-flex rounded-lg  border-2  px-5 py-1.5 text-white'>Explore More</div>
+                </Link>
+              </div>
+            
           </div>
         </div>
       </div>
